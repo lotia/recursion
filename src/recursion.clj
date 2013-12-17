5e42e2a60499a974a8ae8d894a71f1bb00c12abd
Ali Asad Lotia <ali.asad.lotia@gmail.com>
2013-12-17 00:37:44 +0000

    Implemented seq-max and longest-sequence

--- src/recursion.clj
+++ src/recursion.clj
@@ -23,10 +23,15 @@
    :else (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (<= (count seq-1) (count seq-2))
+    seq-2
+    seq-1))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) nil
+   (singleton? a-seq) (first a-seq)
+   :else (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
   [:-])
